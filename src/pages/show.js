db998c65cbc990647e59342b6f5d10025e21fccc
Sakinah Freeman <freemansakinah@gmail.com>
2023-01-25 21:12:28 -0500

    removed extra letter

--- src/pages/show.js
+++ src/pages/show.js
@@ -9,7 +9,7 @@ function Show(props) {
         <div>
 
             <h2> License Details </h2>
-            <h3> License Number:g { license.number } </h3>
+            <h3> License Number: { license.number } </h3>
 
             <h3> First Name:     { license.firstName } </h3>
             <h3> Last Name:      { license.lastName } </h3>
